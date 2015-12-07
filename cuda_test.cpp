3f815a68a9a0aef018777e78671ef61b3b2f8485
Myzhar <myzhar@robot-home.it>
2015-12-07 14:54:44 +0100

    Minor modification

--- cuda_test.cpp
+++ cuda_test.cpp
@@ -11,7 +11,14 @@ int main( )
     cout << "CUDA enabled devices: " << devCount << endl;
 
     if( devCount<1 )
+    {
+        cout << endl << "No GPU support. Possible causes:" << endl;
+        cout << " * System does not have a CUDA enabled card." << endl;
+        cout << " * No CUDA driver installed." << endl;
+        cout << " * OpenCV compiled before CUDA toolkit installation" << endl;
+        cout << " * OpenCV compiled with CUDA flag disabled" << endl << endl;
         return 0;
+    }
 
     for( int i=0; i<devCount; i++ )
     {
