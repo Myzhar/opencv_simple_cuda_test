c36631bc9b23e29acf99806eaccbdef0b2c03b9b
Myzhar <myzhar@gmail.com>
2016-03-29 11:32:19 +0200

    Minor fixes

--- cuda_test.cpp
+++ cuda_test.cpp
@@ -4,7 +4,7 @@
 #include <opencv2/gpu/gpu.hpp>
 using namespace cv::gpu;
 #elif (CV_MAJOR_VERSION==3)
-#include <opencv2/cudaarithm.hpp>
+#include <opencv2/core/cuda.hpp>
 using namespace cv::cuda;
 #endif
 
@@ -16,16 +16,17 @@ using namespace std;
 int main( )
 {
     cout << endl;
-    cout << "OpenCV version:   " << CV_VERSION << endl;
+    cout << "OpenCV version"<< endl;
+    cout << "--------------"<< endl;
     cout << "Major version:    " << CV_MAJOR_VERSION << endl;
     cout << "Minor version:    " << CV_MINOR_VERSION << endl;
     cout << "Subminor version: " << CV_SUBMINOR_VERSION << endl << endl;
   
     int devCount;
 
-    devCount= getCudaEnabledDeviceCount();
+    devCount = getCudaEnabledDeviceCount();
 
-    cout << "CUDA enabled devices: " << devCount << endl;
+    cout << "* Total CUDA enabled devices: " << devCount << endl;
 
     if( devCount<1 )
     {
@@ -44,12 +45,17 @@ int main( )
         cout  << endl << "Device #" << i << ":" << endl;
         cout << "==========" << endl;
 
-        cout << devInfo.name() << endl;
-        cout << "Tot memory: " << devInfo.totalMemory()/(1024*1024) << "MB" << endl;
-        cout << "Free memory: " << devInfo.freeMemory()/(1024*1024) << "MB" << endl;
+        printShortCudaDeviceInfo(i);
+        cout << "* Tot memory: " << devInfo.totalMemory()/(1024*1024) << "MB" << endl;
+        cout << "* Free memory: " << devInfo.freeMemory()/(1024*1024) << "MB" << endl;
+        cout << "-" << endl;
+        cout << "* Complete information:" << endl;
+        printCudaDeviceInfo(i);
+
+
     }
 
-    cout  << endl << "CUDA device active: " << getDevice() << endl  << endl;
+    cout  << "* active CUDA device id: " << getDevice() << endl  << endl;
 
     return 0;
 }
